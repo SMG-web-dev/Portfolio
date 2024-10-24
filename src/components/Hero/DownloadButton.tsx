ed802b14490691fbaf33a827fa45823dfc5ed348
SMG-hash <143341055+SMG-web-dev@users.noreply.github.com>
2024-10-24 09:13:27 +0200

    Update

--- src/components/Hero/DownloadButton.tsx
+++ src/components/Hero/DownloadButton.tsx
@@ -15,7 +15,7 @@ const DownloadButton: React.FC<DownloadButtonProps> = ({ controls }) => {
       className="flex justify-center"
     >
       <a
-        href="./public/SMG-DEV_CV.pdf"
+        href="/public/SMG-DEV_CV.pdf"
         download
         className="inline-flex items-center justify-center w-40 h-10 sm:w-48 sm:h-12 text-sm sm:text-base font-semibold text-white bg-fern-green rounded-full shadow-lg transition-all duration-300 ease-in-out hover:bg-brunswick-green hover:shadow-xl transform hover:-translate-y-1 focus:outline-none focus:ring-2 focus:ring-fern-green focus:ring-opacity-50"
       >
